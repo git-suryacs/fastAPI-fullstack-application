c16eba3835f2cc1300f0e3f36a55bc6c2ce7289b
git-suryacs <2suryacs@gmail.com>
2023-05-16 18:33:12 +0100

    Deleting todo using todo_id

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -4,7 +4,7 @@ sys.path.append("..")
 from starlette.responses import RedirectResponse
 from starlette import status
 from typing import Optional
-from fastapi import Depends, HTTPException, APIRouter, Request,Form
+from fastapi import Depends, HTTPException, APIRouter, Request, Form
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -31,17 +31,21 @@ def get_db():
     finally:
         db.close()
 
-@router.get("/",response_class=HTMLResponse)
-async def read_all_by_user(request: Request,db:Session=Depends(get_db)):
+
+@router.get("/", response_class=HTMLResponse)
+async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
     todos = db.query(models.Todos).filter(models.Todos.owner_id == 1).all()
-    return templates.TemplateResponse("home.html",{"request":request,"todos":todos})
+    return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
 
-@router.get("/add-todo",response_class=HTMLResponse)
-async def add_new_todo(request:Request):
-    return templates.TemplateResponse("add-todo.html",{"request":request})
 
-@router.post("/add-todo",response_class=HTMLResponse)
-async def create_todo(request:Request,title:str=Form(...),description:str = Form(...),priority:int = Form(...),db:Session=Depends(get_db)):
+@router.get("/add-todo", response_class=HTMLResponse)
+async def add_new_todo(request: Request):
+    return templates.TemplateResponse("add-todo.html", {"request": request})
+
+
+@router.post("/add-todo", response_class=HTMLResponse)
+async def create_todo(request: Request, title: str = Form(...), description: str = Form(...), priority: int = Form(...),
+                      db: Session = Depends(get_db)):
     todo_model = models.Todos()
     todo_model.title = title
     todo_model.description = description
@@ -50,10 +54,38 @@ async def create_todo(request:Request,title:str=Form(...),description:str = Form
     todo_model.owner_id = 1
     db.add(todo_model)
     db.commit()
-    return RedirectResponse(url="/todos",status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
 
-@router.get("/edit-todo/{todo_id}",response_class=HTMLResponse)
-async def edit_todo(request:Request,todo_id:int,db:Session=Depends(get_db)):
+
+@router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
     todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
 
-    return templates.TemplateResponse("edit-todo.html",{"request":request,"todo":todo})
+    return templates.TemplateResponse("edit-todo.html", {"request": request, "todo": todo})
+
+
+@router.post("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo_commit(request: Request, todo_id: int, title: str = Form(...), description: str = Form(...),
+                           priority: int = Form(...), db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+@router.get("/delete/{todo_id}")
+async def delete_todo(request:Request,todo_id:int,db:Session=Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).filter(models.Todos.owner_id == 1).first()
+
+    if todo_model is None:
+        return RedirectResponse(url="/todos",status_code=status.HTTP_302_FOUND)
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+    db.commit()
+    return RedirectResponse(url="/todos",status_code=status.HTTP_302_FOUND)
+
+
+
+
