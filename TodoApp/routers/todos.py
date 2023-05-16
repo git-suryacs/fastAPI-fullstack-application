de7a36aaf152167ee3aa75ce69ad0148cf941b0d
git-suryacs <2suryacs@gmail.com>
2023-05-16 17:34:34 +0100

    Edit todo

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -1,9 +1,10 @@
 import sys
 
 sys.path.append("..")
-
+from starlette.responses import RedirectResponse
+from starlette import status
 from typing import Optional
-from fastapi import Depends, HTTPException, APIRouter, Request
+from fastapi import Depends, HTTPException, APIRouter, Request,Form
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -31,14 +32,28 @@ def get_db():
         db.close()
 
 @router.get("/",response_class=HTMLResponse)
-async def read_all_by_user(request: Request):
-    return templates.TemplateResponse("home.html",{"request":request})
+async def read_all_by_user(request: Request,db:Session=Depends(get_db)):
+    todos = db.query(models.Todos).filter(models.Todos.owner_id == 1).all()
+    return templates.TemplateResponse("home.html",{"request":request,"todos":todos})
 
 @router.get("/add-todo",response_class=HTMLResponse)
 async def add_new_todo(request:Request):
-    return templates.TemplateResponse("add-todo.html",{"request":request})\
+    return templates.TemplateResponse("add-todo.html",{"request":request})
 
+@router.post("/add-todo",response_class=HTMLResponse)
+async def create_todo(request:Request,title:str=Form(...),description:str = Form(...),priority:int = Form(...),db:Session=Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1
+    db.add(todo_model)
+    db.commit()
+    return RedirectResponse(url="/todos",status_code=status.HTTP_302_FOUND)
 
 @router.get("/edit-todo/{todo_id}",response_class=HTMLResponse)
-async def edit_todo(request:Request):
-    return templates.TemplateResponse("edit-todo.html",{"request":request})
+async def edit_todo(request:Request,todo_id:int,db:Session=Depends(get_db)):
+    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    return templates.TemplateResponse("edit-todo.html",{"request":request,"todo":todo})
